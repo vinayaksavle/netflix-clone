901ae176766ada545cfc848082e4ed4777ad99c4
Vinayak Savle <32012433+vinayaksavle@users.noreply.github.com>
2020-11-06 23:04:49 +0530

    Removed firebase config values
    Removed firebase config values

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,14 +4,15 @@ import 'firebase/auth';
 // Uncomment this while seeding data i.e, inserting data
 //import { seedDatabase } from '../seed';
 
+//Add your firebase config values
 const config = {
-    apiKey: "AIzaSyAc8-e8kFj_yOj20oFmVao_08NK-n-xGiQ",
-    authDomain: "netflix-clone-88c67.firebaseapp.com",
-    databaseURL: "https://netflix-clone-88c67.firebaseio.com",
-    projectId: "netflix-clone-88c67",
-    storageBucket: "netflix-clone-88c67.appspot.com",
-    messagingSenderId: "528070590746",
-    appId: "1:528070590746:web:5c52eb063543aa465eaca3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
